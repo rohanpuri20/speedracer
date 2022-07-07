8f7a0b28a2f3771ccb38d56d90ec66b400e431e6
rohanpuri20 <108900503+rohanpuri20@users.noreply.github.com>
2022-07-07 16:30:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var canvas;
+var backgroundImage;
+var bgImg;
+var database;
+var form, player;
+var playerCount;
+var gameState
+var car1_img, car2_img, track
+var car1, car2, cars
+function preload() {
+  backgroundImage = loadImage("assets/background.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+}
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+
+}
+
+function draw() {
+  background(backgroundImage);
+  if(playerCount===2){
+    game.update(1)
+  }
+  if(gamestate===1){
+    game.play()
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
